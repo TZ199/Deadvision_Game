70405545a89b9840633d79d3dd5d7438e53f33e5
wu.2482@osu.edu <7758258wsn>
2019-01-28 14:40:35 -0800

    first commit
    wsn

---
+++ The MilkyWay/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float YSpeed = 10;
+    public float XSpeed = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //sprite starts to move immediately
+        MoveInX();
+        if (TouchOrClick())
+        {
+            MoveInY();
+        }
+    }
+    private void FixedUpdate()
+    {
+
+    }
+    //move in X direction
+    void MoveInX()
+    {
+        transform.position += new Vector3(Time.deltaTime * XSpeed, 0, 0);
+    }
+    void MoveInY()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, YSpeed);
+    }
+
+    bool TouchOrClick()
+    {
+        if (Input.GetButtonUp("Jump") || Input.GetMouseButtonDown(0) ||
+                (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Ended))
+            return true;
+        else
+            return false;
+    }
+}
