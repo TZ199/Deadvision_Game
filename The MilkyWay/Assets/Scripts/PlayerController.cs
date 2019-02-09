7f93a736d847e85291ab2c0cf93c37fecbc46c3b
wu.2482@osu.edu <7758258wsn>
2019-02-08 16:11:35 -0800

    added circle game scene
    2/8 meeting

--- The MilkyWay/Assets/Scripts/PlayerController.cs
+++ The MilkyWay/Assets/Scripts/PlayerController.cs
@@ -4,47 +4,44 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float YSpeed = 10;
-    public float XSpeed = 2;
-    public Rigidbody2D rb;
-    // Start is called before the first frame update
+    public float VelocityPerJump = 3;
+    public float XSpeed = 1;
+    Rigidbody2D rb;
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.Space))
+
+        MoveBirdOnXAxis();
+        if (WasTouchedOrClicked())
         {
-            MakeTheObjectsGoUp();
+            BoostOnYAxis();
         }
-    }
-    private void FixedUpdate()
-    {
 
     }
-    //move in X direction
-    void MoveInX()
-    {
-        transform.position += new Vector3(Time.deltaTime * XSpeed, 0, 0);
-    }
-    void MoveInY()
-    {
-        GetComponent<Rigidbody2D>().velocity = new Vector2(0, YSpeed);
-    }
-    public void MakeTheObjectsGoUp()
-    {
-        Physics.gravity = new Vector3(0f, 1f, 0f);
-    }
 
-    bool TouchOrClick()
+    bool WasTouchedOrClicked()
     {
         if (Input.GetButtonUp("Jump") || Input.GetMouseButtonDown(0) ||
-                (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Ended))
+            (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Ended))
             return true;
         else
             return false;
     }
+
+    void MoveBirdOnXAxis()
+    {
+        transform.position += new Vector3(Time.deltaTime * XSpeed, 0, 0);
+    }
+
+    void BoostOnYAxis()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, VelocityPerJump);
+    }
+
+
 }
