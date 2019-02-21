050e4bb69053f988decf1468eba9f230402c2888
wu.2482@osu.edu <7758258wsn>
2019-02-21 15:13:59 -0800

    added spike

--- The MilkyWay/Assets/Scripts/PlyaerController.cs
+++
@@ -1,56 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PlyaerController : MonoBehaviour
-{
-    public float jumpForce = 2.0f;
-    public float speed = 0.5f;     public bool isGrounded;     public Rigidbody2D rb;
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)         {             rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode2D.Impulse);              isGrounded = false;         }
-    }
-    void OnCollisionEnter2D(Collision2D col)
-    {
-        if (col.gameObject.tag == "star")
-        {
-            Destroy(col.gameObject);
-        }         if (col.gameObject.tag == "stick"|| col.gameObject.tag == "switch" || col.gameObject.tag == "floor"|| col.gameObject.tag == "star")
-        {
-            isGrounded = true;
-        }
-        if (col.gameObject.tag == "star")
-        {
-            //pop up the go to next level window
-        }
-        if (col.gameObject.tag == "switch")
-        {
-            //pop up the go to next level window
-            rb.AddForce(new Vector3(speed, 0, 0), ForceMode2D.Impulse);
-        }
-
-
-    }
-    void OnCollisionExit2D(Collision2D col)
-    {
-        if (col.gameObject.tag == "stick" || col.gameObject.tag == "switch" || col.gameObject.tag == "floor" || col.gameObject.tag == "star")
-        {
-            isGrounded = false;
-        }
-    }
-
-    void OnCollisionStay2D(Collision2D col)
-    {
-        if (col.gameObject.tag == "stick")
-        {
-            isGrounded = true;
-        }
-    }
-}
---
+++ The MilkyWay/Assets/Scripts/spikeToDeath.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spikeToDeath : MonoBehaviour
+{
+    public GameObject gameover;
+    public PlayerController player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //player = GameObject.Find("circle player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            gameover.SetActive(true);
+            player.dead = true;
+
+        }
+    }
+}
