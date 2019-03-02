7c4eadc8874edec33e58efaea58dc2453bd85da0
Tianxin <zhoutx0@gmail.com>
2019-03-01 19:20:59 -0800

    Ignore is working now
    get rid of usless changes in every git fetch

---
+++ The MilkyWay/Assets/Scripts/UIManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LoadScene(string name)
+    {
+        Application.LoadLevel(name);
+     }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+}
---
+++ The MilkyWay/Assets/Scripts/rui_switch_1.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class rui_switch_1 : MonoBehaviour
+{
+    public GameObject com;
+    public GameObject sw;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Destroy(com);
+        }
+
+    }
+}
